0228932c68b1c97e73a9a16023385bc295491638
Jay Y <alforoanjay@gmail.com>
2023-03-01 23:04:33 +0000

    Completed Max Minus Min Lab

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -1,5 +1,6 @@
 
 import java.util.List;
+import java.util.*;
 
 public class MaxMinusMin {
     /**
@@ -10,6 +11,9 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        Collections.sort(nums);
+        int max = nums.get(nums.size()-1);
+        int min = nums.get(0);
+        return max-min;
     }
 }
